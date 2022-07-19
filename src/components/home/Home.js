e3bc2508e9b3af2be4f5e57b331521382dd65552
NantoNaku <Yasin-Yilmaz@outlook.de>
2022-07-19 18:20:22 +0200

    Frontend

---
+++ src/components/home/Home.js
@@ -0,0 +1,50 @@
+import "./Home.css";
+import io from "socket.io-client";
+import { useState } from "react";
+import Chat from "../chat/Chat";
+
+const ENDPOINT = process.env.ENDPOINT || "https://localhost:5000";
+
+const socket = io.connect(ENDPOINT);
+
+function Home() {
+  const [username, setUsername] = useState("");
+  const [room, setRoom] = useState("");
+  const [showChat, setShowChat] = useState(false);
+
+  const joinRoom = () => {
+    if (username !== "" && room !== "") {
+      socket.emit("join_room", room);
+      setShowChat(true);
+    }
+  };
+
+  return (
+    <div className="App">
+      {!showChat ? (
+        <div className="joinChatContainer">
+          <h3>Join A Chat</h3>
+          <input
+            type="text"
+            placeholder="Nickname.."
+            onChange={(event) => {
+              setUsername(event.target.value);
+            }}
+          />
+          <input
+            type="text"
+            placeholder="Roomname..."
+            onChange={(event) => {
+              setRoom(event.target.value);
+            }}
+          />
+          <button onClick={joinRoom}>Join A Room</button>
+        </div>
+      ) : (
+        <Chat socket={socket} username={username} room={room} />
+      )}
+    </div>
+  );
+}
+
+export default Home;
